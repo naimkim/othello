58a1f11c3621c0ac89ff164e40cb5ff1b67fe049
naimkim <naimkim2000@naver.com>
2020-11-26 02:11:24 +0900

    11_26_1

--- main.c
+++ main.c
@@ -8,16 +8,14 @@ int draw_othello;//게임판 만들기
 int print_othello();  // 게임자 입력 받기 전 게임판,배치 상태 출력, 게임과정 출력해서 사용자가 알 수 있게 하는거 
 int isGameEnd(); //game 종료 조건 확인
 int isBoardPlay(); //배치가능 칸 확인 
-int turn(); //돌 뒤집고, 돌 개수 검사하 기  
-int check_result(); //결과 출력 
+int turn(); //돌 뒤집고, 돌 개수 검사하 기   
 int garo_check();//가로  낑긴 돌 바꾸기  
 int sero_check();
 int degak1_check(); // y=-x
 int degak2_check(); // y=x
 
 
-void main (void) {
-	int gameboard[N][N];	//필요한 변수들 정의 : WHITE개수 , BLACK개수  ,6*6 게임판 , 입력한 좌표  
+void main (void) {	//필요한 변수들 정의 : WHITE개수 , BLACK개수  ,6*6 게임판 , 입력한 좌표  
 //칸이 비어있음 : 0 
 	int empty = 0;
 //	칸에 검정알이 있음 : 1 : X
@@ -27,7 +25,8 @@ void main (void) {
 
 	int player_cnt;// 현재 turn 돌 개수 
 	int player_t;//맞으면 1 틀리면 0 != == 
-	int game_cnt;//turn 개수 
+	int game_cnt;//turn 개수
+	int pass_cnt=0; 
 	int input1;
 	int input2;
 	
@@ -44,12 +43,12 @@ void main (void) {
 	game_cnt = 1;	
 	while (isGameEnd() == 0) { //game 종료 조건 확인 
 	 	 print_othello(); 
-	 	 if (isBoardPlay() == 0) 
-	 	 	 continue; //두 player 모두 배치가 불가능하면 반복문을 빠져나가야함
+	 	 if (isBoardPlay() == 1) 
+	 	 	{ //두 player 모두 배치가 불가능하면 반복문을 빠져나가야함
 			
  			printf(" out a new %f othello : ", player_t);
   			scanf(" %d %d  ",&input1, &input2); 
-				  	
+				  
 	 	 	if ( input1<= 0 || input1>=6 || input2<=0 || input2 >=6 ) {  
 			//문의주신 출력의 경우, 각 칸에 대해 위의 배열요소 값에 맞게 O 혹은 X 혹은 빈 칸으로 출력을 하면 됩니다. 
 					switch (gameboard[input1][input2])
@@ -66,29 +65,51 @@ void main (void) {
 			
 						default:
 							break;	
-					}
-			else		
+					}}
+			else{
 				printf(" invalid input! (should be less than 6)\n");
 				//turn함수에서  몇개 뒤집었는지cnt세서 출력; 
-				// 턴 바꿈 
-	 	 	 }
-	 	 else(isBoardPlay() == 1) 
+				// 턴 바꿈
+				if(isGameEnd == 0 && player_t == -1) 
+					player_t = 1;
+				else if(isGameEnd == 0 && player_t == 1)
+					player_t = -1; 
+	 	 	 }}
+	 	 else if (isBoardPlay() == 0) 
 			{
 			  pass_cnt++;
+			  continue; 
 			if(isGameEnd == 0 && player_t == -1) 
 				player_t = 1;
 			else if(isGameEnd == 0 && player_t == 1)
 				player_t = -1;
-			else 
-				break; 
+			 
+				 
 			}
 			 
 	//돌 뒤집기   			
 	turn();
 	game_cnt++;
-	}	
-	check_result(); //결과 출력
-	// printf("최종 결과 : 이긴사람 : %c, 이긴 사람 알 개수 : %d", winner, winner_cnt);
+	}
+	if((game_cnt != 0) && (game_cnt % 2 == 0))
+
+        {
+			printf("winner is black");
+			printf("black count is %d", player_cnt);
+			
+        }
+
+        else if(game_cnt % 2 == 1) 
+
+        {	printf("winner is white");
+			printf("white count is %d", player_cnt);
+			
+
+        }
+
+        else
+
+               printf( " 0 \n" );
 }
 
 int draw_board()//게임판 그리는 함수
@@ -130,17 +151,12 @@ int print_othello(int player_cnt, int player_t, int game_cnt, int pass_cnt)
 	a = draw_board();
 	printf("%f",a);
 	//알개수  status : white 2, black 2 
-	if(player_t == white)
+	if(player_t == -1)
 		printf(" STATUS - WHITE : %d, BLACK : %d ", player_cnt, game_cnt-pass_cnt - player_cnt);
-			
+	else if (player_t == 1)
+		printf(" STATUS - WHITE : %d, BLACK : %d ", game_cnt-pass_cnt - player_cnt, player_cnt);		
   	//배치 결과 방향별 뒤집어지는 알 개수, 총뒤집어진 알개수 
-  	if(game_cnt>1)
-	{ printf("::flip result:: \n");
-		 printf("E : %d, W : %d, N : %d, S : %d, NE : %d, SE : %d, NW : %d, SW : %d\n", east, west, north, south, x_en, x_es, x_wn, x_ws);
-		printf("you has flipped %d othellos! ", east+west+north+south+x_en +x_es+x_wn+x_ws+garo_cnt+sero_cnt+degak1_cnt+degak2_cnt);
- 	 }
-	  else
-  			break;
+  	
   	
 	
 	return total_flip;
@@ -184,7 +200,7 @@ while(x==1)
 
 //isboardplay 가 0이면 pass 
 int isBoardPlay(int gameboard[N][N], int flip_cnt)
-  {//배치 가능 칸이 있는지 확인   있으면 0 없으면 1 
+  {//배치 가능 칸이 있는지 확인   있으면 1 없으면 0 
    //알이 없는 칸인가
    int i,j;
    int cnt=0;// cnt 가 36이 되면 배치가능한 칸 없음  
@@ -352,6 +368,7 @@ for (j=0;j< d2-1;j++)
 
 	return (d1+d2);
 }
+
 int degak2_check(int player_t,int input1, int input2, int gameboard[N][N])
 {
  // y=x
@@ -419,9 +436,9 @@ int turn(int player_t, int gameboard[N][N], int game_cnt, int input1, int input2
 	int sero_cnt;
 	int degak1_cnt;
 	int degak2_cnt;
-	 int player_cnt;
-	 int playerAcnt; 
-	 int a,b,c,d; 
+	int player_cnt;
+	int playerAcnt; 
+	int a,b,c,d; 
  
  while ( gameboard[input1][input2] == player_t)//22 --11,12,13,23,33,32,31,21
  {
@@ -473,7 +490,7 @@ int turn(int player_t, int gameboard[N][N], int game_cnt, int input1, int input2
  }	
 playerAcnt = east+west+north+south+x_en +x_es+x_wn+x_ws;//뒤집은 돌 개수 세고 빠져나오기   
  //낑긴 돌 바꾸고 갯수를 return시키는 함수
-  
+  //int player_t,int input1, int input2, int gameboard[N][N]
 a= garo_check();  
 b= sero_check();
 c= degak1_check(); 
@@ -487,16 +504,7 @@ player_cnt = a+ b+ c+d + playerAcnt;
  	 }
 		
 	return player_cnt; 
-	
-	
-//return 값이 여러개이길 바랬지만 불가능 하니까 포인터 쓰겠삼~ 
- //	return gameboard[N][N]; 
- //	return player_cnt;//현재 turn player가 몇개 뒤집었는지  
+	 
 }
 
 
-int check_result()
-{ //결과 출력 
-//winner은 어 
-printf("최종 결과 : 이긴사람 : %c, 이긴 사람 알 개수 : %d", winner, winner_cnt);
-}
